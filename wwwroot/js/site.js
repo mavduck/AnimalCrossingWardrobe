cfd12144816c2009beecb61c20fc99ca9eb9002e
katelyn <kmsinn@iastate.edu>
2023-03-14 16:40:18 -0500

    Replace selectlist with dropdownlist, show villager icons on dropdown, filtered villager favorites, add villager search on dropdown"

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,55 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function villagerSelect(newSelectedId) {
+    var input, initialSelectedElement, newSelectedElement, newSelectedElementValue, dropdownElement;
 
-// Write your JavaScript code.
+    input = document.getElementById("selectedVillagerNameInput");
+
+    initialSelectedVillagerName = input.value;
+
+    if(initialSelectedVillagerName != ""){
+        initialSelectedElement = document.getElementById(initialSelectedVillagerName+"ListItem");
+    }
+    else{
+        initialSelectedElement = document.getElementById("NoVillagerListItem");
+    }
+
+    if (initialSelectedElement != null) {
+        initialSelectedElement.classList.remove("active");
+    }
+
+    newSelectedElement = document.getElementById(newSelectedId);
+    newSelectedElement.classList.add("active");
+    newSelectedElementValue = newSelectedElement.getAttribute("data-value");
+
+    dropdownElement = document.getElementById("selectedVillagerDropdownCover");
+    dropdownElement.innerHTML = newSelectedElementValue == "" ? "Choose Villager" : newSelectedElement.innerHTML;
+
+    var images = dropdownElement.getElementsByTagName("img");
+
+    if(images.length > 0){
+        images[0].classList.remove("villager-icon-fit");
+        images[0].classList.add("villager-icon-sm");
+    }
+
+    input.value = newSelectedElementValue;
+}
+
+function villagerSearch() {
+    var inputElement, searchString, dropdownElement, villagerItems, i;
+
+    inputElement = document.getElementById("villagerDropdownSearch");
+    searchString = inputElement.value.toLowerCase();
+    dropdownElement = document.getElementById("villagerDropdown");
+    villagerItems = dropdownElement.getElementsByClassName("villagerListItem");
+    
+    for(i = 0; i < villagerItems.length; i++){
+        var villagerItem = villagerItems[i];
+        if(villagerItem.getAttribute("data-value").toLowerCase().startsWith(searchString)){
+            villagerItem.style.display = "";
+        }
+        else{
+            villagerItem.style.display = "none";
+        }
+    }
+
+
+}
